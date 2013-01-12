ff09e875a0b903181ce9092689b18dd440debb17
Predatorian <wahlen001@aol.com>
2013-01-12 18:51:41 +0100

    Updated license info and project infos

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ZoomLevelGenerator
 ==================
+
+actual status:
+This project started on 11.01.13 and can't do anything at the moment.
+
+
+target functionality:
+Generating map tiles for higher zoom levels for salem map tiles to use them with the google maps api.
--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
-#include <QtCore/QCoreApplication>
+/* 
+*  This file is part of the ZoomLevelGenerator. 
+*  Copyright (C) 2013 Alexander Wahlen <wahlen001@aol.com>. 
+* 
+*  This file is part of ZoomLevelGenerator.
+*  
+*  ZoomLevelGenerator is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*  
+*  ZoomLevelGenerator is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  
+*  A copy the GNU General Public License version 3 is distributed along 
+*  with the source tree of which this file is a part in the file 
+*  `./COPYING'. If it is missing for any reason, 
+*  please see <http://www.gnu.org/licenses/>.
+*/
 
+
+#include <QtCore/QCoreApplication>
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
     return a.exec();
 }
